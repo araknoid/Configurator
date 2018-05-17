f53b1fc222b544ce1b21bdce80f877d845d84839
Carlo Corti <araknoid@gmail.com>
2018-05-17 16:33:53 +0200

    Add unique constraint on the name of the configuration

--- src/main/java/com/araknoid/configurator/configuration/Configuration.java
+++ src/main/java/com/araknoid/configurator/configuration/Configuration.java
@@ -1,5 +1,6 @@
 package com.araknoid.configurator.configuration;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -11,6 +12,7 @@ public class Configuration {
     @GeneratedValue
     private Long id;
 
+    @Column(unique = true)
     private String name;
 
     private String value;
